8da553d40c6165769d8c92ef397fd72d749e8c23
Yadel Lopez <yopez83@gmail.com>
2018-10-25 22:08:27 -0400

    Add tests GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,15 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/Todo');
 const {User} = require('./../models/User');
 
+const todos = [
+    { 'text' : 'First test todo' },
+    { 'text': 'Second test todo' }
+];
+
 beforeEach((done) => {
-    Todo.deleteMany({}).then(() => done());
+    Todo.deleteMany({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());;
 });
 
 describe('POST /todos', () => {
@@ -25,7 +32,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -35,18 +42,30 @@ describe('POST /todos', () => {
 
     it('should not create todo with invalid data', (done) => {
         request(app)
-        .post('/todos')
-        .send({})
-        .expect(400)
-        .end((err, res) => {
-            if(err){
-                return done(err);
-            }
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
 
-            Todo.find().then((todos) => {
-                expect(todos.length).toBe(0)
-                done();
-            }).catch((e) => done(e));
-        })
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(2)
+                    done();
+                }).catch((e) => done(e));
+            })
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBeGreaterThan(0);
+            })
+            .end(done);
     });
 });
