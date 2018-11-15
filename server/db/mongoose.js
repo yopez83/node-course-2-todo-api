579146979fe1624577b23f1f545c72e4e62c9f56
Yadel Lopez <yopez83@gmail.com>
2018-11-14 23:44:56 -0500

    Removing useMongoClient

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
-    useMongoClient: true,
     useNewUrlParser: true
 });
 
